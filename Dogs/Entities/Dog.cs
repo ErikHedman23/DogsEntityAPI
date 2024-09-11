1bddf0ced22bb95100e9141eb19c5b612ab22061
Erik Hedman <erikdavid.hedman21@outlook.com>
2024-09-10 20:49:04 -0400

    Adjusted some comments

--- Dogs/Entities/Dog.cs
+++ Dogs/Entities/Dog.cs
@@ -3,9 +3,9 @@
     public class Dog
     {
         public int Id { get; set; } //autoincrementing id as the primary key
-        public required string Name { get; set; } = string.Empty; //nvarchar(max) - not ideal, but just for test purposes
-        public string Home { get; set; } = string.Empty; //nvarchar(max) - not ideal, but just for test purposes
+        public required string Name { get; set; } = string.Empty; //nvarchar(50)
+        public string Home { get; set; } = string.Empty; //nvarchar(50)
         public int Age { get; set; } // int
-        public string Description { get; set; } = string.Empty; //nvarchar(max) - not ideal, but just for test purposes
+        public string Description { get; set; } = string.Empty; //nvarchar(100)
     }
 }
